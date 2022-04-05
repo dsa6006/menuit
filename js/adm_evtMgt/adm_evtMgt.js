64c05a4fec502e8f835f18c9c7c4a0605e9bd8e2
SoRaang <dsc0320@hotmail.com>
2022-04-05 10:31:12 +0900

    sdaf

--- js/adm_evtMgt/adm_evtMgt.js
+++ js/adm_evtMgt/adm_evtMgt.js
@@ -1,10 +1,33 @@
 const btnSurvey = document.querySelector('.btn_survey');
 const btnAdd = document.querySelector('.btn_add');
 
+
+const frmSurvey = document.getElementById('addSurvey');
+const frmEvent = document.getElementById('addEvent');
+
 btnSurvey.addEventListener('click', function() {
 
 });
 
 btnAdd.addEventListener('click', function() {
 
+});
+
+const btnUpload = document.querySelector('.btn_upload');
+const fileUploader = document.getElementById('fileAttach');
+
+let triggerClick = new MouseEvent('click', {
+    bubbles: true,
+    cancelable: true,
+    view: window
+});
+
+btnUpload.addEventListener('click', function() {
+    let triggerClick = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+    });
+
+    fileUploader.dispatchEvent(triggerClick);
 });

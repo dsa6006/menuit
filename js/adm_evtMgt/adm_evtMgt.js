e849842c2865b7049eb3fe924c8ade5b4f6c6731
SoRaang <dsc0320@hotmail.com>
2022-04-05 11:28:16 +0900

    admin event 3

--- js/adm_evtMgt/adm_evtMgt.js
+++ js/adm_evtMgt/adm_evtMgt.js
@@ -1,17 +1,29 @@
 const btnSurvey = document.querySelector('.btn_survey');
 const btnAdd = document.querySelector('.btn_add');
 
-
+const popWindow = document.getElementById('pop_add');
 const frmSurvey = document.getElementById('addSurvey');
 const frmEvent = document.getElementById('addEvent');
 
 btnSurvey.addEventListener('click', function() {
-
+    popWindow.dataset.show = true;
+    frmSurvey.dataset.show = true;
+    frmEvent.dataset.show = false;
 });
 
 btnAdd.addEventListener('click', function() {
-
+    popWindow.dataset.show = true;
+    frmSurvey.dataset.show = false;
+    frmEvent.dataset.show = true;
 });
+
+var btnClose = popWindow.querySelectorAll('.btn_cancel');
+
+for(var i = 0; i < btnClose.length; i ++) {
+    btnClose[i].addEventListener('click', function() {
+        popWindow.dataset.show = false;
+    });
+}
 
 const btnUpload = document.querySelector('.btn_upload');
 const fileUploader = document.getElementById('fileAttach');
